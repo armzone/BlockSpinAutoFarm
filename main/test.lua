76ba06ca6a452764f057e332ae653188cf210fa0
armzone <armkung285@gmail.com>
2025-06-19 17:54:52 +0700

    Update test.lua

--- main/test.lua
+++ main/test.lua
@@ -11,7 +11,7 @@ local humanoid = char:WaitForChild("Humanoid")
 
 local ATMFolder = Workspace:WaitForChild("Map"):WaitForChild("Props"):WaitForChild("ATMs")
 local moving = false
-local speed = 1
+local speed = 5
 
 -- ตรวจสอบว่า ATM พร้อมใช้งาน
 local function IsATMReady(atm)
